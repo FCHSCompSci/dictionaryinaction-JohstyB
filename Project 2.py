7d0564fce8f4093abea63632b09b261cb9fe92db
Johsty Eric Bach <johsty.bach@stu.jefferson.kyschools.us>
2018-09-24 08:55:35 -0400

    Final version of Project 2.

--- Project 2.py
+++ Project 2.py
@@ -1,10 +1,86 @@
-dictionaryTest = {
+#Johsty Bach
+#Project 2: Dictionaries
+#Finished on 9/24/18
+
+#Dictionary that is edited in the main code
+myDict = {
 	"test1":"testA",
         "test2":"testB",
 }
 
-for key, value in dictionaryTest.items():
-	print("%s: %s" % (key, value))
+
+
+'''Tests'''
+
+#Testing coverting a dictionary to a list
+dictTest = list_key_value = [ [k,v] for k, v in dict.items(myDict) ]
+#print(list_key_value)
+
+
+#Testing more stuff
+def list1():
+        dictTest1 = []
+        count = 0
+        for i in dictTest:
+                if count % 2 == 0:
+                        dictTest1.append(i)
+                        count += 1
+        return dictTest1
+
+def list2():
+        dictTest2 = []
+        count = 0
+        for i in dictTest:
+                if count % 2 == 1:
+                        dictTest2.append(i)
+                        count += 1
+        return dictTest2
+
+
+
+'''Functions'''
+
+#Function to add a new key and value
+def addKeyValue():
+        key = input("Enter a key to be added: ")
+        if key in myDict:
+                print("That key already exists. ")
+        else:
+                value = input("Enter the value for this key: ")
+                myDict[key] = value
+        return myDict
+
+#Function to remove a key and it's value
+def removeKeyValue(dictDelete):
+        if dictDelete in myDict:
+                del myDict[dictDelete]
+        else:
+                print("That key does not exist. ")
+        return myDict
+
+#Function to change a key and it's value
+def changeKeyValue():
+        key = input("Enter a key to have it's value changed: ")
+        if key in myDict:
+                value = input("Enter the new value for this key: ")
+                myDict[key] = value
+        else:
+                print("That key does not exist. ")
+
+#Function to print the dictionary
+def printDict():
+        for key, value in myDict.items():
+                print("%s: %s" % (key, value))
+
+
+
+
+#Prints the dictionary as it is before the main code starts, if anything is there
+printDict()
+
+
+
+
 
 #Main Code
 while True:
@@ -13,24 +89,19 @@ while True:
         while True:
             cmd2 = input("Update or View? U or V: ")
             if cmd2 == ("U"):
-                cmd3 = input("Would you like to add a new key and value, remove an existing key and value, or change an existing one? A or R or C: ")
+                cmd3 = input("Would you like to [A]dd a new key and value, [R]emove an existing key and value, or [C]hange an existing key and value?: ")
                 if cmd3 == ("A"):
-                        key = input("Enter a new key to add: ")
-                        value = input("Enter a value for the new key: ")
-                        dictionaryTest[key] = value
+                        addKeyValue()
                 elif cmd3 == ("R"):
-                        dictDelete = input("Enter a key to be removed: ")
-                        del dictionaryTest[dictDelete]
+                        dictDelete = input("Please input a key to be deleted: ")
+                        removeKeyValue(dictDelete)
                 elif cmd3 == ("C"):
-                        key = input("Enter a key to have it's value changed: ")
-                        value = input("Enter the new value for this key: ")
-                        dictionaryTest[key] = value
+                        changeKeyValue()
                 else:
                         print("Please enter a valid response." )
                 break
             elif cmd2 == ("V"):
-                for key, value in dictionaryTest.items():
-                    print("%s: %s" % (key, value))
+                printDict()
                 break
             else:
                 print("Please enter a valid response." )
@@ -39,4 +110,3 @@ while True:
         break
     else:
         print("Please enter a valid response." )
-    
